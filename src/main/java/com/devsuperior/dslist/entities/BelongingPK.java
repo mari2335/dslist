8714417b500fd25a36b87c470c313f891c261b59
Mariana Saito <marianast12@hotmail.com>
2023-10-25 22:29:55 -0400

    Domain model

--- src/main/java/com/devsuperior/dslist/entities/BelongingPK.java
+++ src/main/java/com/devsuperior/dslist/entities/BelongingPK.java
@@ -1,9 +1,20 @@
 package com.devsuperior.dslist.entities;
 
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
 import java.util.Objects;
 
+@Embeddable
 public class BelongingPK {
+
+    @ManyToOne
+    @JoinColumn(name = "game_id")
     private Game game;
+
+    @ManyToOne
+    @JoinColumn(name = "game_list_id")
     private GameList list;
 
     public BelongingPK() {}
